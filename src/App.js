ebf86b871ce90bd8cf12cc9a26cadb2597c09894
Student <student@d17-327e-pc-19.ipa.ii.agh.edu.pl>
2025-12-14 15:14:42 +0100

    Naprawde drugi commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let title = 'Wall-E';
+
+    const [title, setTitle] = useState('Wall-E');
+
 
     function handleChange(event) {
-        console.log(event.target.value);
+
+        setTitle(event.target.value);
+    }
+
+    let message ='';
+    
+    if (title.length === 0) {
+      message = "Wpisz coś...";
+    } else if (title.length < 3) {
+      message = "Krótki tytuł";
+    } else if (title.length < 20) {
+      message = "Dobry tytuł";
+    } else {
+      message = "Za długi tytuł";
     }
 
     return (
+
         <div>
             <h1>My favourite movies to watch</h1>
+
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+
+            <input type="text" value={title} onChange={handleChange}/>
+
+            <div>{message}</div>
         </div>
+
     );
+
 }
 
 export default App;
